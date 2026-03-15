55d954f8d31df3cf5a42b39502601e23c4635467
asaf-sardas <asafsardas1@gmail.com>
2026-03-15 21:58:35 +0200

    Integrate new routers for authentication, content, search, and users into the main FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
+from api.routes import auth_router, content_router, search_router, users_router
 
 app = FastAPI(title="video semantic search api",
  description="API for semantic search of videos", )
+
+
+app.include_router(auth_router.router)
+app.include_router(content_router.router)
+app.include_router(search_router.router)
+app.include_router(users_router.router)
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
-@app.get("/api/v1/content/{id}/status")
-def get_status(id: str):
-    return {"content_id": id, "status": "pending"}
