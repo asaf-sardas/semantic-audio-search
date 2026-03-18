59aea64575792aa1f4b8539f32c34791546a0305
asaf-sardas <asafsardas1@gmail.com>
2026-03-18 19:58:57 +0200

    Connect user and auth routes to their respective services

--- backend/api/routes/auth_router.py
+++ backend/api/routes/auth_router.py
@@ -1,11 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from database import get_db
+from schemas.user_schema import UserCreate, UserLogin, UserRead
+from services.user_service import register_user, login_user
 
 router = APIRouter(prefix="/api/v1/auth", tags=["Authentication"])
 
 @router.post("/register")
-def register():
-    return {"message": "User registered successfully"}
+def register(payload: UserCreate, db: Session = Depends(get_db)) -> UserRead:
+    return register_user(db, payload)
 
 @router.post("/login")
-def login():
-    return {"message": "User logged in successfully"}
+def login(payload: UserLogin, db: Session = Depends(get_db)) -> UserRead:
+    return login_user(db, payload)
